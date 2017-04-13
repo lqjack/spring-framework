e5cdb400491a1a2db36aa35f33ebd8554f3c7573
lqjaclee <lqjacklee@126.com>
2017-04-13 22:30:14 +0800

    update reference

--- spring-web/src/main/java/org/springframework/http/client/AsyncClientHttpRequest.java
+++ spring-web/src/main/java/org/springframework/http/client/AsyncClientHttpRequest.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.client.reactive.*;
 import org.springframework.util.concurrent.ListenableFuture;
 
 /**
@@ -32,7 +33,7 @@ import org.springframework.util.concurrent.ListenableFuture;
  * @author Arjen Poutsma
  * @since 4.0
  * @see AsyncClientHttpRequestFactory#createAsyncRequest
- * @deprecated as of Spring 5.0, in favor of {@link org.springframework.web.reactive.function.client.ClientRequest}
+ * @deprecated as of Spring 5.0, in favor of {@link org.springframework.http.client.reactive.ClientHttpRequest}
  */
 @Deprecated
 public interface AsyncClientHttpRequest extends HttpRequest, HttpOutputMessage {
